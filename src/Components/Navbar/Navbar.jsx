543a1edc3b244e2ccd9014a653e995167cc1d0ca
Wwright91 <wisdomwright-martin@pursuit.org>
2024-07-26 00:20:55 -0400

    Navbar styling
    1. Installed bootstrap
    2. Added logo img to project
    3. Added styling to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+const hero = "../../../public/ww-logo.jpeg";
+
+import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="Navbar">
+      <NavLink to="/">
+        <img src={hero} alt="hero" />
+      </NavLink>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <NavLink to="/about">About</NavLink>
+              <NavLink to="/projects">Projects</NavLink>
+              <NavLink to="/contact">Contact</NavLink>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
