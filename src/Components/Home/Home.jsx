07e50a79c5e218899f7006306102f719a1f3e71c
Wwright91 <wisdomwright-martin@pursuit.org>
2024-07-28 23:01:46 -0400

    Home page changes
    1. Added img of myself to project
    2. Added styling to home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+const hero = "../../../public/wisdom-wright.jpeg";
+
+import "./Home.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="Home">
+      <div>
+        <img src={hero} alt="Wisdom Wright" />
+        <h1>Wisdom Wright</h1>
+        <p>
+          Fullstack Developer passionate about building impactful web
+          applications
+        </p>
+      </div>
+      <div className="Home__Buttons">
+        <NavLink to="/about">
+          <button>Learn More About Me</button>
+        </NavLink>
+        <NavLink to="/projects">
+          <button>See My Work</button>
+        </NavLink>
+        <NavLink to="/contact">
+          <button>Get In Touch</button>
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
