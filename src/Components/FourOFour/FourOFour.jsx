a653280fff1e146220686ed9895d594f9098aac7
Wwright91 <wisdomwright-martin@pursuit.org>
2024-08-12 12:42:04 -0400

    Changed svg from Vite

--- src/Components/FourOFour/FourOFour.jsx
+++ src/Components/FourOFour/FourOFour.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const FourOFour = () => {
-  return <div>FourOFour</div>;
+  return (
+    <section className="fouroFour">
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <a href="/">Go back to Home</a>
+    </section>
+  );
 };
 
 export default FourOFour;
