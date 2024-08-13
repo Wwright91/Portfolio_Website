6c3fcc2f3ffca091534e1d0aed7b24bbd3715b5a
Wwright91 <wisdomwright-martin@pursuit.org>
2024-08-13 00:09:58 -0400

    Github Calendar
    1. Installed and implemented react-github-calendar
    2. Fixed media queries for home page buttons

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,19 +1,80 @@
 import React from "react";
 import "./About.scss";
-import Skills from "../Skills/Skills"
+import Skills from "../Skills/Skills";
+import GitHubCalendar from "react-github-calendar";
 
 const About = () => {
+  const selectLastHalfYear = (contributions) => {
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth() + 1;
+    const shownMonths = 8;
+    return contributions.filter((activity) => {
+      const date = new Date(activity.date);
+      const monthOfDay = date.getMonth();
+
+      return (
+        date.getFullYear() === currentYear &&
+        monthOfDay > currentMonth - shownMonths &&
+        monthOfDay <= currentMonth
+      );
+    });
+  };
+
   return (
     <section className="About">
       <div className="About__container">
         <h2 className="About__title">About Me</h2>
         <p className="About__description">
-          I am a dedicated Software Engineer specializing in full-stack web development. My commitment to continuous learning keeps me updated with the latest industry trends and best practices. I enjoy solving coding challenges and currently have over 6000 points on <a href="https://pursuit.codetrack.dev/fellow/592" target="_blank" rel="noopener noreferrer">CodeTrack</a>.
+          I am a <strong style={{color: "#F25F25"}}><i>dedicated</i></strong> Software Engineer specializing in full-stack web
+          development. My commitment to <strong style={{color: "#F25F25"}}><i>continuous learning</i></strong> keeps me updated
+          with the latest industry trends and best practices. I enjoy solving
+          coding challenges and currently have over 6000 points on{" "}
+          <strong>
+          <a
+            href="https://pursuit.codetrack.dev/fellow/592"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            CodeTrack
+            </a>
+            </strong>
+          .
         </p>
         <p className="About__description">
-          I love web development and enjoy creating projects that address real-world problems. Outside of coding, I have a keen interest in trying new foods and cooking, which fuels my creativity. I also enjoy solving sudoku puzzles and reading in my spare time.
+          I love <strong style={{color: "#F25F25"}}><i>web development</i></strong> and enjoy creating projects that address
+          real-world problems. Outside of coding, I have a keen interest in
+          trying new foods and cooking, which fuels my <strong style={{color: "#F25F25"}}><i>creativity</i></strong>. I also enjoy
+          solving sudoku puzzles and reading in my spare time.
         </p>
-        <Skills/>
+        <h2>Coding Contributions This Year</h2>
+        <GitHubCalendar
+          id="About__calendar"
+          style={{
+            display: "flex",
+            width: "800px",
+            alignItems: "center",
+            padding: "20px",
+            backgroundColor: "#F6F6F4"
+          }}
+          username="Wwright91"
+          blockSize={16}
+          blockMargin={3}
+          blockRadius={9}
+          hideMonthLabels={true}
+          colorScheme="light"
+          transformData={selectLastHalfYear}
+          fontSize={14}
+          theme={{
+            light: ["hsl(0, 0%, 90%)", "orange"],
+            dark: ["hsl(0, 0%, 92%)", "orange"],
+          }}
+          labels={{
+            totalCount: "{{count}} contributions this year",
+          }}
+          weekStart={1}
+        />
+        <br />
+        <Skills />
       </div>
     </section>
   );
