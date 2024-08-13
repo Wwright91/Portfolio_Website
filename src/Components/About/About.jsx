7842d922c24f0a1e049c78a67ea8a83105104f4f
Wwright91 <wisdomwright-martin@pursuit.org>
2024-08-13 00:23:49 -0400

    Fixed container width in About component

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
   const selectLastHalfYear = (contributions) => {
     const currentYear = new Date().getFullYear();
     const currentMonth = new Date().getMonth() + 1;
-    const shownMonths = 8;
+    const shownMonths = 9;
     return contributions.filter((activity) => {
       const date = new Date(activity.date);
       const monthOfDay = date.getMonth();
@@ -25,42 +25,55 @@ const About = () => {
       <div className="About__container">
         <h2 className="About__title">About Me</h2>
         <p className="About__description">
-          I am a <strong style={{color: "#F25F25"}}><i>dedicated</i></strong> Software Engineer specializing in full-stack web
-          development. My commitment to <strong style={{color: "#F25F25"}}><i>continuous learning</i></strong> keeps me updated
-          with the latest industry trends and best practices. I enjoy solving
-          coding challenges and currently have over 6000 points on{" "}
+          I am a{" "}
+          <strong style={{ color: "#F25F25" }}>
+            <i>dedicated</i>
+          </strong>{" "}
+          Software Engineer specializing in full-stack web development. My
+          commitment to{" "}
+          <strong style={{ color: "#F25F25" }}>
+            <i>continuous learning</i>
+          </strong>{" "}
+          keeps me updated with the latest industry trends and best practices. I
+          enjoy solving coding challenges and currently have over 6000 points on{" "}
           <strong>
-          <a
-            href="https://pursuit.codetrack.dev/fellow/592"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            CodeTrack
+            <a
+              href="https://pursuit.codetrack.dev/fellow/592"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              CodeTrack
             </a>
-            </strong>
+          </strong>
           .
         </p>
         <p className="About__description">
-          I love <strong style={{color: "#F25F25"}}><i>web development</i></strong> and enjoy creating projects that address
-          real-world problems. Outside of coding, I have a keen interest in
-          trying new foods and cooking, which fuels my <strong style={{color: "#F25F25"}}><i>creativity</i></strong>. I also enjoy
-          solving sudoku puzzles and reading in my spare time.
+          I love{" "}
+          <strong style={{ color: "#F25F25" }}>
+            <i>web development</i>
+          </strong>{" "}
+          and enjoy creating projects that address real-world problems. Outside
+          of coding, I have a keen interest in trying new foods and cooking,
+          which fuels my{" "}
+          <strong style={{ color: "#F25F25" }}>
+            <i>creativity</i>
+          </strong>
+          . I also enjoy solving sudoku puzzles and reading in my spare time.
         </p>
         <h2>Coding Contributions This Year</h2>
         <GitHubCalendar
-          id="About__calendar"
           style={{
             display: "flex",
-            width: "800px",
+            width: "auto",
             alignItems: "center",
             padding: "20px",
-            backgroundColor: "#F6F6F4"
+            backgroundColor: "#F6F6F4",
+            color: "orange",
           }}
           username="Wwright91"
           blockSize={16}
           blockMargin={3}
           blockRadius={9}
-          hideMonthLabels={true}
           colorScheme="light"
           transformData={selectLastHalfYear}
           fontSize={14}
